904aa16c9f42ed9bf97b277d8b9025014a6795ac
Nate Boxer <nboxer@usc.edu>
2023-03-04 14:59:44 -0800

    Part 1 Completed - Tree Traversals tested and working

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -1,6 +1,5 @@
 #ifndef RECCHECK
 //if you want to add any #includes like <iostream> you must do them here (before the next endif)
-
 #endif
 
 #include "equal-paths.h"
@@ -8,11 +7,31 @@ using namespace std;
 
 
 // You may add any prototypes of helper functions here
-
+bool imbalance(Node* root);
+bool isLeaf(Node* root);
 
 bool equalPaths(Node * root)
 {
     // Add your code below
+    if (!root) return true;
+    if (imbalance(root)) return false;
+    
+    bool left = true, right = true;
+    if (root->left) left = equalPaths(root->left);
+    if (root->right) right = equalPaths(root->right);
 
+    return left && right;
 }
 
+bool imbalance(Node* root)
+{    
+    if (root->left && root->right) {
+        return isLeaf(root->left) != isLeaf(root->right);
+    }
+    return false;
+}
+
+bool isLeaf(Node* root)
+{
+    return !root->left && !root->right;
+}
